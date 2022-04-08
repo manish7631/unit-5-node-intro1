f0853badd368b518668ff57439db4d72e952c1c2
manishfw13 <manish_fw13_347@masai.school>
2022-04-08 20:38:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
+  const products = ["Android", "Blackberry", "iPhone", "Windows Phone"];
+  const products2 = ["Samsung", "HTC", "Micromax", "Apple"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        {products.map((e) => {
+          return <li>{e}</li>
+        })}
+      </ul>
+      <h1>Mobile Manufactures</h1>
+      <ul>
+         {products2.map((e) => {
+           return <li>{e}</li>
+         })}
+      </ul>
     </div>
   );
 }
